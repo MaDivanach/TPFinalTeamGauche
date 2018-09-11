24e925ecd82c1fd92a6b4fc679c08c29c595a461
Unknown <antoine.jcavalier@gmail.com>
2018-09-11 12:45:41 +0200

    formation

---
+++ angular/src/app/model/Formation.ts
@@ -0,0 +1,77 @@
+export class Formation {
+
+  constructor(private _id?: number, private _nom?: string, private _dateDebut?: Date, private _dateFin?: Date, private _gestionnaire?: Gestionnaire, private _videoProjecteur?: VideoProjecteur, private _salle?: Salle, private _stagiaires?: Set<Stagiaires>, private _sessions?: Set<Session>) {
+  }
+
+  get id(): number {
+    return this._id;
+  }
+
+  set id(value: number) {
+    this._id = value;
+  }
+
+  get nom(): string {
+    return this._nom;
+  }
+
+  set nom(value: string) {
+    this._nom = value;
+  }
+
+  get dateDebut(): Date {
+    return this._dateDebut;
+  }
+
+  set dateDebut(value: Date) {
+    this._dateDebut = value;
+  }
+
+  get dateFin(): Date {
+    return this._dateFin;
+  }
+
+  set dateFin(value: Date) {
+    this._dateFin = value;
+  }
+
+  get gestionnaire(): Gestionnaire {
+    return this._gestionnaire;
+  }
+
+  set gestionnaire(value: Gestionnaire) {
+    this._gestionnaire = value;
+  }
+
+  get videoProjecteur(): VideoProjecteur {
+    return this._videoProjecteur;
+  }
+
+  set videoProjecteur(value: VideoProjecteur) {
+    this._videoProjecteur = value;
+  }
+
+  get salle(): Salle {
+    return this._salle;
+  }
+
+  set salle(value: Salle) {
+    this._salle = value;
+  }
+
+  get stagiaires(): Set<Stagiaires> {
+    return this._stagiaires;
+  }
+
+  set stagiaires(value: Set<Stagiaires>) {
+    this._stagiaires = value;
+  }
+
+  get sessions(): Set<Session> {
+    return this._sessions;
+  }
+
+  set sessions(value: Set<Session>) {
+    this._sessions = value;
+  }
+}
--- angular/src/app/service/formation.service.ts
+++ angular/src/app/service/formation.service.ts
@@ -1,9 +1,50 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from "@angular/core";
+import {Formation} from "../model/Formation";
 
 @Injectable({
   providedIn: 'root'
 })
 export class FormationService {
 
-  constructor() { }
+  url: string = 'http://localhost:8080/demo';
+  header: HttpHeaders;
+
+  constructor(private http: HttpClient) {
+    this.header = new HttpHeaders({
+      'Content-type': 'application/json',
+      'Authorization': 'Basic ' + btoa('olivier:olivier')
+    });
+  }
+
+  public list(): Observable<Formation[]> {
+    return this.http.get<Formation[]>(`${this.url}/rest/formation/`, {headers: this.header});
+  }
+
+  public delete(id: number): Observable<any> {
+    return this.http.delete(`${this.url}/rest/formation/${id}`, {headers: this.header});
+  }
+
+  public findById(id: number): Observable<Formation> {
+    return this.http.get<Formation>(`${this.url}/rest/formation/${id}`, {headers: this.header});
+  }
+
+  public save(formation: Formation): Observable<any> {
+    if (formation.id) {
+      return this.http.put(`${this.url}/rest/formation/`, formation, {headers: this.header});
+    } else {
+      const o = {
+        id: formation.id,
+        nom: formation.nom,
+        dateDebut: formation.dateDebut,
+        dateFin: formation.dateFin,
+        gestionnaire: formation.gestionnaire,
+        videoProjecteur : formation.videoProjecteur,
+        salle: formation.salle,
+        stagiaires: formation.stagiaires,
+        sessions: formation.sessions
+      };
+      console.log(o);
+      return this.http.post(`${this.url}/rest/formation/`, o, {headers: this.header});
+    }
+  }
 }
