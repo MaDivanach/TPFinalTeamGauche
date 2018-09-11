c59c0c948790d0e9ebb637238d1339ac7a7d2547
Unknown <antoine.jcavalier@gmail.com>
2018-09-11 10:10:54 +0200

    rest control formation

---
+++ java/src/main/java/com/sopra/TPFinal/web/controller/rest/FormationRestController.java
@@ -0,0 +1,121 @@
+package com.sopra.TPFinal.web.controller.rest;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import com.fasterxml.jackson.annotation.JsonView;
+import com.sopra.TPFinal.model.Formation;
+import com.sopra.TPFinal.repositories.FormationRepository;
+import com.sopra.TPVolAngular.model.Vol;
+import com.sopra.TPVolAngular.model.view.JsonViews;
+import com.sopra.TPVolAngular.repositories.VolRepository;
+
+@RestController
+@RequestMapping("/rest/formation")
+public class FormationRestController {
+
+	@Autowired
+	private FormationRepository formationRepository;
+
+	@JsonView(JsonViews.Common.class)
+	@GetMapping(path = { "/", "" })
+	public ResponseEntity<List<Formation>> findAll() {
+		return new ResponseEntity<>(formationRepository.findAll(), HttpStatus.OK);
+	}
+
+	@JsonView(JsonViews.StagiaireInFormation.class)
+	@GetMapping(path = { "/stagiaireinformation" })
+	public ResponseEntity<Optional<Formation>> StagiaireInFormation(Long id) {
+		return new ResponseEntity<>(formationRepository.StagiaireInFormation(id), HttpStatus.OK);
+	}
+	@JsonView(JsonViews.SessionInFormation.class)
+	@GetMapping(path = { "/sessionInFormation" })
+	public ResponseEntity<Optional<Formation>> SessionInFormation(Long id) {
+		return new ResponseEntity<>(formationRepository.SessionInFormation(id), HttpStatus.OK);
+	}
+	
+	@PostMapping(path = { "/", "" })
+	public ResponseEntity<Void> createFormation(@Valid @RequestBody Formation formation, BindingResult br,
+			UriComponentsBuilder uCB) {
+		ResponseEntity<Void> response = null;
+		if (br.hasErrors()) {
+			response = new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		} else {
+			formationRepository.save(formation);
+			HttpHeaders header = new HttpHeaders();
+			header.setLocation(uCB.path("/rest/formation/{id}").buildAndExpand(formation.getId()).toUri());
+			response = new ResponseEntity<Void>(header, HttpStatus.CREATED);
+		}
+		return response;
+	}
+
+	@GetMapping(value = "/{id}")
+	@JsonView(JsonViews.Common.class)
+	public ResponseEntity<Formation> findById(@PathVariable(name = "id") Long id) {
+		Optional<Formation> opt = formationRepository.findById(id);
+		ResponseEntity<Formation> response = null;
+		if (opt.isPresent()) {
+			response = new ResponseEntity<>(opt.get(), HttpStatus.OK);
+		} else {
+			response = new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		}
+		return response;
+	}
+
+	@JsonView(JsonViews.Common.class)
+	@PutMapping(path = { "/", "" })
+	public ResponseEntity<Formation> update(@Valid @RequestBody Formation formation, BindingResult br) {
+		if (br.hasErrors() || formation.getId() == null) {
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		}
+		Optional<Formation> opt = formationRepository.findById(formation.getId());
+		if (opt.isPresent()) {
+			// update possible
+			Formation FormationEnBase = opt.get();
+			FormationEnBase.setDateDebut(formation.getDateDebut());
+			FormationEnBase.setDateFin(formation.getDateFin());
+			FormationEnBase.setGestionnaire(formation.getGestionnaire());
+			FormationEnBase.setHeureVideoProjecteur(formation.getVideoProjecteur());
+			FormationEnBase.setArrivee(vol.getArrivee());
+			FormationEnBase.setDepart(vol.getDepart());
+			FormationEnBase.setReservations(vol.getReservations());
+			FormationEnBase.setEscales(vol.getEscales());
+			FormationEnBase.setCompagnieAeriennes(vol.getCompagnieAeriennes());
+			return new ResponseEntity<Vol>(volEnBase, HttpStatus.OK);
+		} else {
+			// pas de vol
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		}
+
+	}
+
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable(name = "id") Long id) {
+		Optional<Vol> opt = volRepository.findById(id);
+		ResponseEntity<Void> response = null;
+		if (opt.isPresent()) {
+			volRepository.deleteById(id);
+			response = new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		} else {
+			response = new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		}
+		return response;
+	}
+}
