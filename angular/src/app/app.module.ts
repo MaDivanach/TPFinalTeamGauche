ce186fb245edd2ac3273bf11d37f8557eec12a8f
Unknown <mathilde.divanach@gmail.com>
2018-09-11 10:16:27 +0200

    Création components et services

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -7,11 +7,28 @@ import {FormsModule} from '@angular/forms';
 import {RouterModule} from '@angular/router';
 import {HttpClientModule} from '@angular/common/http';
 import {routes} from '../route';
+import {MaterielComponent} from './materiel/materiel.component';
+import {FormateurComponent} from './formateur/formateur.component';
+import {MatiereComponent} from './matiere/matiere.component';
+import {SessionComponent} from './session/session.component';
+import {PlanningComponent} from './planning/planning.component';
+import {FormationComponent} from './planning/formation.component';
+import {FormateurService} from './service/formateur.service';
+import {FormationService} from './service/formation.service';
+import {MaterielService} from './service/materiel.service';
+import {SessionService} from './service/session.service';
+import {UserService} from './service/user.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    MaterielComponent,
+    FormateurComponent,
+    MatiereComponent,
+    SessionComponent,
+    PlanningComponent,
+    FormationComponent
   ],
   imports: [
     BrowserModule,
@@ -20,7 +37,13 @@ import {routes} from '../route';
     RouterModule.forRoot(routes),
     HttpClientModule
   ],
-  providers: [],
+  providers: [
+    FormateurService,
+    FormationService,
+    MaterielService,
+    SessionService,
+    UserService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {
