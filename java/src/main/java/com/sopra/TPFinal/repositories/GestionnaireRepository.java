c51a3f6f7a92a616a3cec81df4819a8e365e9254
Unknown <antoine.jcavalier@gmail.com>
2018-09-12 15:37:28 +0200

    java rests &form

---
+++ java/src/main/java/com/sopra/TPFinal/repositories/GestionnaireRepository.java
@@ -0,0 +1,11 @@
+package com.sopra.TPFinal.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.sopra.TPFinal.model.Gestionnaire;
+
+public interface GestionnaireRepository extends JpaRepository<Gestionnaire, Long> {
+
+
+
+}
