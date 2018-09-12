dcf5f3b66c4edae5ce1cc821dec87a74c56a0aea
Thibault <thibault.dufeu@gmail.com>
2018-09-12 12:54:50 +0200

    commit header on services

--- angular/src/app/service/expertise.service.ts
+++ angular/src/app/service/expertise.service.ts
@@ -11,32 +11,36 @@ export class ExpertiseService {
 
   url: string = 'http://localhost:8080/Projet';
 
-  /*headers: HttpHeaders;*/
+  header: HttpHeaders;
 
   constructor(private http: HttpClient) {
+    this.header = new HttpHeaders({
+      'Content-type': 'application/json',
+      'Authorization': 'Basic '
+    });
   }
 
   public list(): Observable<Expertise[]> {
-    return this.http.get<Expertise[]>(`${this.url}/rest/expertise/`);
+    return this.http.get<Expertise[]>(`${this.url}/rest/expertise/`, {headers: this.header});
   }
 
   public delete(id: number): Observable<any> {
-    return this.http.delete(`${this.url}/rest/expertise/${id}`);
+    return this.http.delete(`${this.url}/rest/expertise/${id}`, {headers: this.header});
   }
 
   public findById(id: number): Observable<Expertise> {
-    return this.http.get<Expertise>(`${this.url}/rest/expertise/${id}`);
+    return this.http.get<Expertise>(`${this.url}/rest/expertise/${id}`, {headers: this.header});
   }
 
   public save(expertise: Expertise): Observable<any> {
     if (expertise.key) {
-      return this.http.put(`${this.url}/rest/expertise/`, expertise);
+      return this.http.put(`${this.url}/rest/expertise/`, expertise, {headers: this.header});
     } else {
       const o = {
         key: expertise.key
       };
       console.log(o);
-      return this.http.post(`${this.url}/rest/expertise`, o);
+      return this.http.post(`${this.url}/rest/expertise`, o, {headers: this.header});
     }
   }
 
