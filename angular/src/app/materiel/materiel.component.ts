b97f72676aba74db4b5c9502d91729834f67f3f9
Larry Silverstein <Ictyospore@protonmail.com>
2018-09-13 09:07:26 -0700

    Dernier commit

--- angular/src/app/materiel/materiel.component.ts
+++ angular/src/app/materiel/materiel.component.ts
@@ -2,6 +2,9 @@ import {Component, Input, OnInit} from '@angular/core';
 import {Materiel} from '../model/materiel';
 import {_mymateriel} from './tableauMateriel';
 import {MaterielService} from '../service/materiel.service';
+import {Ordinateur} from '../model/ordinateur';
+import {VideoProjecteur} from '../model/videoProjecteur';
+import {Salle} from '../model/salle';
 
 @Component({
   selector: 'app-materiel',
@@ -11,6 +14,10 @@ import {MaterielService} from '../service/materiel.service';
 export class MaterielComponent implements OnInit {
 
   materiels: Materiel[];
+  ordinateurs: Ordinateur[];
+  salles: Salle[];
+  videoprojecteurs: VideoProjecteur[];
+
 
   constructor(private materielService: MaterielService) {
   }
@@ -18,6 +25,9 @@ export class MaterielComponent implements OnInit {
 
   ngOnInit() {
     this.list();
+/*    this.listO();
+    this.listV();
+    this.listS();*/
   }
 
   public list() {
@@ -25,6 +35,24 @@ export class MaterielComponent implements OnInit {
       this.materiels = resp;
     }, error => console.log(error));
   }
+
+/*  public listO() {
+    this.materielService.listO().subscribe(resp => {
+      this.ordinateurs = resp;
+    }, error => console.log(error));
+  }
+
+  public listV() {
+    this.materielService.listV().subscribe(resp => {
+      this.videoprojecteurs = resp;
+    }, error => console.log(error));
+  }
+
+  public listS() {
+    this.materielService.listS().subscribe(resp => {
+      this.salles = resp;
+    }, error => console.log(error));
+  }*/
 
   public delete(id: number) {
     return this.materielService.delete(id).subscribe(resp => {
